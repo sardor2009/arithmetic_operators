ed165cc3d3a8e9ce440159911bbb1bd687cd048b
sardor <ruziqulovsardor3@gmail.com>
2022-08-08 13:48:10 +0500

    code

--- bin/arithmetic006.dart
+++ bin/arithmetic006.dart
@@ -1,9 +1,9 @@
-import 'dart:math';
 // Create function func with arguments
 // the task condition is in the Readme file
+import 'dart:math';
 num func(int a, int b){
     return pow(a, b);
 }
 void main() {
-    print(func(2 , 6));
+    print(func(2, 4));
 }
