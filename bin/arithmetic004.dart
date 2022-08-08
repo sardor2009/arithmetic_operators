9b39e967535dd97022b89d6650b2010d230e507d
sardor <ruziqulovsardor3@gmail.com>
2022-08-08 13:54:55 +0500

    code

--- bin/arithmetic004.dart
+++ bin/arithmetic004.dart
@@ -1,9 +1,9 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 int func(int a, int b){
-    return a / b;
+    return a ~/ b;
 }
 
 void main() {
-    print(8 / 4);
+    print(func(12, 6));
 }
